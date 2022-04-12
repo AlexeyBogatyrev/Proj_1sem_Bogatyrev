74ef2534eb25abe5d4a6862ef217cd8dfad5a8f7
mariia2206 <alex.bog04@mail.ru>
2022-04-12 16:49:30 +0300

    26.03.2022

---
+++ figures/circle/code.py
@@ -0,0 +1,13 @@
+def circle_perimeter():
+    default_radius = 5
+    dlina = 2 * 3.14 * default_radius
+
+    return dlina
+def circle_area():
+    default_radius = 5
+    s = default_radius ** 2 * 3.14
+
+    return s
+
+
+
---
+++ figures/square/code.py
@@ -0,0 +1,9 @@
+def square_perimeter():
+    a = 15
+    perim = a * 4
+    return perim
+def square_area():
+    a = 15
+    area = a * a
+    return area
+
