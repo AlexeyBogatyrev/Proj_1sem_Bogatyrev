ba8c1cc416d24755fd135e7ac7925a61ce40e84e
Alexey Bogatyrev <alex.bog04@mail.ru>
2022-04-25 22:02:26 +0300

    25.04.2022

---
+++ main.py
@@ -0,0 +1,15 @@
+from figures import circle_area
+circle_area(2)
+from figures import circle_perimeter
+circle_perimeter()
+from figures import square_area
+square_area()
+from figures import square_perimeter
+square_perimeter()
+from figures import triangle_perimeter
+triangle_perimeter()
+from figures import triangle_area
+triangle_area(8, 5, 7)
+
+
+
