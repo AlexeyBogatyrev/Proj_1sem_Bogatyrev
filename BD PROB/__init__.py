60ced594835ee0538d97f003d20dcbf40b714b3c
Alexey Bogatyrev <alex.bog04@mail.ru>
2022-05-14 16:18:59 +0300

    Rabota c BD

--- BD PROB/__init__.py
+++ BD PROB/__init__.py
@@ -1,3 +1,4 @@
+
 import sqlite3 as sq
 with sq.connect('saper.db') as con:
  cur = con.cursor()
@@ -8,4 +9,22 @@ with sq.connect('saper.db') as con:
  sex INTEGER NOT NULL DEFAULT 1,
  old INTEGER,
  score INTEGER
- )""") 
+ )""")
+cur.execute("SELECT * FROM users")
+con.commit()
+cur.execute("SELECT * FROM users WHERE sex > 2")
+con.commit()
+cur.execute("SELECT * FROM users WHERE score < 1000")
+con.commit()
+cur.execute("SELECT * FROM users ORDER BY score DESC LIMIT 1")
+con.commit()
+cur.execute("SELECT * FROM users WHERE old BETWEEN 18 AND 20")
+con.commit()
+cur.execute("UPDATE users SET old = 20  WHERE old = 19")
+con.commit()
+cur.execute("UPDATE users SET score = score + 300  WHERE score < 1000")
+con.commit()
+cur.execute("UPDATE users SET score = score + 100  WHERE old >= 20")
+con.commit()
+cur.execute("DELETE FROM users WHERE score < 1000")
+con.commit()
